cdc59a88a800c7fef95d3290d5b164813b7f88e8
kameshyadavky <kameshyadavky@gmail.com>
2024-09-16 18:47:42 +0530

    feat(setup): added dependencies for typescript and tailwindcss

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css' // this will be used for all components, treat is as app theme
+
+// add dom to root element
+ReactDOM.createRoot(document.getElementById('root')!).render(
+    // React.StrictMode is a tool for highlighting potential problems in app
+    // https://react.dev/reference/react/StrictMode
+    <React.StrictMode>
+        {/* Your components will go here */}
+        <p>Kamesh Yadav</p>
+    </React.StrictMode>,
+)
---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from "vite";
+import { compression } from 'vite-plugin-compression2'
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [
+        react(),
+        compression(
+            {
+                include: [/\.(js|css|html|json|svg)$/],
+                deleteOriginalAssets: true
+            }
+        )
+    ],
+    build: {
+        // ...
+        minify: true,
+        sourcemap: false
+    },
+});
